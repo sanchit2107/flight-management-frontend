645d2dcecff249dc7a27aa16290fa3b691be26be
Tarun Kumar <46074444+tarun9777@users.noreply.github.com>
2020-09-30 20:19:58 +0530

    login and registration done

---
+++ src/app/user-login/user-login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: "app-user-login",
+  templateUrl: "./user-login.component.html",
+  styleUrls: ["./user-login.component.css"],
+})
+export class UserLoginComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  failure = { value: false };
+  userData = null;
+
+  loginForm = this.formBuilder.group({
+    userId: [null, [Validators.required]],
+    password: [null, [Validators.required, Validators.minLength(8)]],
+  });
+
+  ngOnInit(): void {
+    this.failure.value = false;
+  }
+
+  submit(){
+    this.userService.userLogin(this.loginForm.value).subscribe(
+      (data) => {
+        this.userData = data;
+        this.failure.value = false;
+        console.log(this.userData.userId);
+        localStorage.setItem("userId",this.userData.userId);
+      },
+      (error) => {
+        this.failure.value = true;
+        this.loginForm.reset();
+      }
+    );
+  }
+
+
+}
---
+++ src/app/user.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  private baseUrl = "http://localhost:8086/user";
+
+  httpHeaders = new HttpHeaders({
+    "Content-Type": "application/json",
+  });
+
+  options = { headers: this.httpHeaders };
+
+  addUser(user):Observable<any> {
+    console.log(JSON.stringify(user));
+    return this.http.post<any>(`${this.baseUrl}/addUser`,JSON.stringify(user),this.options).pipe(
+      catchError(this.errorHandler)
+    );
+  
+  }
+
+  userLogin(user):Observable<any> {
+    console.log(JSON.stringify(user));
+    return this.http
+      .post<any>(this.baseUrl + "/userLogin", JSON.stringify(user), this.options)
+      .pipe(catchError(this.errorHandler));
+    // throwError('error message');
+  }
+
+  errorHandler(error:HttpErrorResponse) {
+    let errorMessage = "";
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}
