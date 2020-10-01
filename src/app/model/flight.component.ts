5d757325a683411fd10683d49599b7ac2c40732e
sanchit2107 <sanchit2107@gmail.com>
2020-10-01 19:55:14 +0530

    Adding MODEL class for FLIGHT-DETAILS

---
+++ src/app/model/flight.component.ts
@@ -0,0 +1,11 @@
+export class FlightDetails {
+  sourceAirport: string;
+  destinationAirport: string;
+  availableSeats: number;
+  departureDate: string;
+  arrivalDate: string;
+  arrivalTime: string;
+  departureTime: string;
+  flightVendor: string;
+  price: number;
+}
