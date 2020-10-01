b662dca618d765dc806b4e4eaa2bfa0915dac9b0
sanchit2107 <sanchit2107@gmail.com>
2020-10-01 20:13:03 +0530

    Updating the route path for ViewAllFlights

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AddFlightDetailsComponent } from './add-flight-details/add-flight-detai
 import { ErrorComponent } from "./error/error.component";
 import { UserLoginComponent } from "./user-login/user-login.component";
 import { UserRegisterComponent } from "./user-register/user-register.component";
+import { ViewAllFlightDetailsComponent } from './view-all-flight-details/view-all-flight-details.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/userLogin", pathMatch: "full" },
   { path: "userLogin", component: UserLoginComponent },
   { path: "addUser", component: UserRegisterComponent },
   { path: "error/:message", component: ErrorComponent },
-  { path: "addFlightDetails", component: AddFlightDetailsComponent}
+  { path: "addFlightDetails", component: AddFlightDetailsComponent},
+  { path: "allFlightsDetails", component: ViewAllFlightDetailsComponent}
 ];
 
 @NgModule({
