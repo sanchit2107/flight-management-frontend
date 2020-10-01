cc0e2fa13abd3dc2a58da775ea1fde63e2902723
Tarun Kumar <46074444+tarun9777@users.noreply.github.com>
2020-10-01 13:59:28 +0530

    fixed

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-import { AdminService } from '../service/admin.service';
+import { AdminService } from '../services/admin.service';
 
 
 @Component({
--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AbstractControl, FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-import { UserService } from "../service/user.service";
+import { UserService } from "../services/user.service";
 
 @Component({
   selector: "app-user-register",
